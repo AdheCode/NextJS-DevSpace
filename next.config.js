72f0d069d9143e0a9431f071e6cc911dfe2d31b2
AdheCode <adhep86@gmail.com>
2021-12-02 16:34:38 +0700

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    node_env: process.env.NODE_ENV,
+  },
 }
---
+++ pages/api/search.js
@@ -0,0 +1,35 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import getConfig from "next/config";
+
+const { serverRuntimeConfig } = getConfig();
+
+export default function handler(req, res) {
+  let posts;
+
+  if (serverRuntimeConfig.node_env === "production") {
+    // Fetch from cache
+    posts = require('../../cache/data').posts;
+  } else {
+    const files = fs.readdirSync(path.join("posts"));
+    posts = files.map((file) => {
+      const slug = file.replace('.md', '');
+      const markdownWithMeta = fs.readFileSync(
+        path.join("posts", file),
+        "utf8"
+      );
+      const { data: frontmatter } = matter(markdownWithMeta);
+      return { frontmatter, slug };
+    });
+  }
+
+  const results = posts.filter(
+    ({ frontmatter: { title, excerpt, category } }) =>
+      title.toLowerCase().indexOf(req.query.q) != -1 ||
+      excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
+      category.toLowerCase().indexOf(req.query.query) != -1
+  );
+
+  res.status(200).json(JSON.stringify({results}));
+}
